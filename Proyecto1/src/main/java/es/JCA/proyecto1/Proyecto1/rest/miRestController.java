857f95da50e3ce534022848b31109ba0da3a5a79
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-12-24 11:49:28 -0600

    REST Controller
     Curso Spring Boot. REST Controller. Vídeo 4

---
+++ Proyecto1/src/main/java/es/JCA/proyecto1/Proyecto1/rest/miRestController.java
@@ -0,0 +1,14 @@
+package es.JCA.proyecto1.Proyecto1.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//EL REST CONTROLLER INDICA QUE LA CLASE ES UN CONTROLLADOR REST
+@RestController
+public class miRestController {
+    //DEFINE QUE EL METODO SALUDO MANEJA LAS SOLICITUDES EN ESE ENDPOINT CON UN RETURN ENJ UNA CADENA DE TEXTO
+    @GetMapping("/")
+    public String saludo(){
+        return "Hola Mundo";
+    }
+}
