23730375e9c030a85026247e2d0e094da5997eea
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-12-24 12:07:03 -0600

    Controller + Thymeleaf + basic HTML
     Curso Spring Boot. Controller. Vídeo 5

--- Proyecto1/src/main/java/es/JCA/proyecto1/Proyecto1/rest/miRestController.java
+++ Proyecto1/src/main/java/es/JCA/proyecto1/Proyecto1/rest/miRestController.java
@@ -1,14 +1,18 @@
 package es.JCA.proyecto1.Proyecto1.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //EL REST CONTROLLER INDICA QUE LA CLASE ES UN CONTROLLADOR REST
-@RestController
+//@RestController
+
+//
+@Controller
 public class miRestController {
     //DEFINE QUE EL METODO SALUDO MANEJA LAS SOLICITUDES EN ESE ENDPOINT CON UN RETURN ENJ UNA CADENA DE TEXTO
     @GetMapping("/")
     public String saludo(){
-        return "Hola Mundo";
+        return "index";
     }
 }
