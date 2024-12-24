42dbf5fc6e811d42e5feb35703a3a5345d2c8d73
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-12-24 12:21:10 -0600

    Update miRestController

--- Proyecto1/src/main/java/es/JCA/proyecto1/Proyecto1/rest/miRestController.java
+++ Proyecto1/src/main/java/es/JCA/proyecto1/Proyecto1/rest/miRestController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 //
 @Controller
 public class miRestController {
-    //DEFINE QUE EL METODO SALUDO MANEJA LAS SOLICITUDES EN ESE ENDPOINT CON UN RETURN ENJ UNA CADENA DE TEXTO
+    //DEFINE QUE EL METODO SALUDO MANEJA LAS SOLICITUDES EN ESE ENDPOINT CON UN RETURN EN UNA CADENA DE TEXTO ó si es un
+    //Controller, podrá regresar un HTML como repsuesta en v ez de un XML o JSON
     @GetMapping("/")
     public String saludo(){
         return "index";
