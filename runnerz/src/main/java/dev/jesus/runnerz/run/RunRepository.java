97823e0b0a69e40b88e301514e4adff5d19d5b22
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2025-01-08 19:21:06 -0600

    REST API PART 1/2
     Building web applications in Java with Spring Boot 3 – Tutorial

---
+++ runnerz/src/main/java/dev/jesus/runnerz/run/RunRepository.java
@@ -0,0 +1,42 @@
+package dev.jesus.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Run findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst()
+                .get();
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Monday",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Tuesday",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                5,
+                Location.INDOOR));
+    }
+}
